8229434b2576d287ac89e2957fad5d9491e3d8ef
MMojoCoder <173496186+MMojoCoder@users.noreply.github.com>
2025-02-19 09:33:50 -0800

    production changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 ﻿from flask import Flask, render_template
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def chat():
     return render_template('/chat.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    serve(app, host="0.0.0.0",port=5001)
